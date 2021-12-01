f9ce664629757960a737d38e7abca04166865873
Jasmin Frei <jasmin.frei@hotmail.com>
2021-12-01 09:07:29 +0100

    solve day 1

--- 2021/src/Day01.kt
+++ 2021/src/Day01.kt
@@ -1,7 +1,44 @@
 import java.io.File
 
 fun main() {
-    val numbers = File("resources/input1.txt")
+    val dummyInput = """
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+    """.trimIndent()
+
+    /*val numbers = dummyInput.split("\n").map(String::toInt)*/
+
+    val numbers = File("resources/input01.txt")
         .readLines()
+        .map(String::toInt)
+
+    val amountPt1 = numbers
+        .zipWithNext()
+        .map { pair ->
+            pair.first < pair.second
+        }
+        .filter { it }
+        .size
+
+    println(amountPt1)
+
+    val amountPt2 = numbers
+        .windowed(3)
+        .map { window -> window[0] + window[1] + window[2] }
+        .zipWithNext()
+        .map { pair ->
+            pair.first < pair.second
+        }
+        .filter { it }
+        .size
+    println(amountPt2)
 
 }
