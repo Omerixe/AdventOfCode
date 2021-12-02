64034ce679f7b93e29417fc94276515458fdbe9f
Jasmin Frei <jasmin.frei@hotmail.com>
2021-12-02 08:35:20 +0100

    2021 solve day 2

---
+++ 2021/src/Day02.kt
@@ -0,0 +1,48 @@
+import java.io.File
+
+fun main() {
+    val dummyInput = """
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+    """.trimIndent()
+
+/*val input = dummyInput.split("\n")*/
+
+    val input = File("resources/input02.txt")
+        .readLines()
+
+    var depthPt1 = 0
+    var horPosPt1 = 0
+    input.forEach {
+        val parts = it.split(" ")
+        val amount = parts.last().toInt()
+        when (parts.first()) {
+            "forward" -> horPosPt1 += amount
+            "up" -> depthPt1 -= amount
+            "down" -> depthPt1 += amount
+        }
+    }
+    println("Depth: $depthPt1, horizontal Position: $horPosPt1, Product: ${depthPt1 * horPosPt1}")
+
+    var depthPt2 = 0
+    var horPosPt2 = 0
+    var aim = 0
+
+    input.forEach {
+        val parts = it.split(" ")
+        val amount = parts.last().toInt()
+        when (parts.first()) {
+            "forward" -> {
+                horPosPt2 += amount
+                depthPt2 += aim * amount
+            }
+            "up" -> aim -= amount
+            "down" -> aim += amount
+        }
+    }
+    println("Depth: $depthPt2, horizontal Position: $horPosPt2, Product: ${depthPt2 * horPosPt2}")
+}
