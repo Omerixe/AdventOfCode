5d39f41dc5923c1280898d6832ea7062ec330a30
Jasmin Frei <jasmin.frei@hotmail.com>
2021-12-08 11:58:04 +0100

    2021 solve day 8

---
+++ 2021/src/Day08.kt
@@ -0,0 +1,106 @@
+import java.io.File
+
+fun main() {
+    val dummyInput = """
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """.trimIndent()
+
+    //val input = dummyInput.split("\n").map { it.split(" | ").map { it.split(" ") } }
+
+    val input = File("resources/input08.txt").readLines().map { it.split(" | ").map { it.split(" ") } }
+
+    val result = input.map { line ->
+        line[1].map { it.length }
+    }.flatten().filter { it == 2 || it == 4 || it == 3 || it == 7 }.size
+    println("Result Pt1: $result")
+
+    val resultPt2 = input.map { line ->
+        val charMap = mutableMapOf<String, String>()
+
+        // 1 and 7 share two characters and the one that is left, is a
+        line.first()
+            .filter { it.length == 2 || it.length == 3 }
+            .map { it.split("") }
+            .flatten()
+            .groupingBy { it }
+            .eachCount()
+            .forEach {
+                if (it.value == 1) charMap["a"] = it.key
+            }
+
+        // Count how often each char appears
+        val charCount = line.first().map {
+            it.split("")
+        }.flatten().groupingBy { it }.eachCount()
+
+        // Use some heuristics to map some of the chars as they have unique counts
+        // a and b both have 8 characters, but as we already have a, we know that the other is c
+        charCount.forEach { entry ->
+            when (entry.value) {
+                4 -> charMap["e"] = entry.key
+                6 -> charMap["b"] = entry.key
+                9 -> charMap["f"] = entry.key
+                8 -> {
+                    if (entry.key != charMap["a"]) {
+                        charMap["c"] = entry.key
+                    }
+                }
+            }
+        }
+
+        // 4 & 8 share b, c, d, f and we already know b, c, f which means we can find out d
+        line.first()
+            .filter { it.length == 4 || it.length == 7 }
+            .map { it.split("") }
+            .flatten()
+            .groupingBy { it }
+            .eachCount()
+            .forEach {
+                if (it.value == 2 && it.key != charMap["b"] && it.key != charMap["c"] && it.key != charMap["f"]) charMap["d"] =
+                    it.key
+            }
+
+        // Now that we know d, we can find out g as both share a count of 6 times
+        charCount.forEach { entry ->
+            if (entry.value == 7 && entry.key != charMap["d"]) charMap["g"] = entry.key
+        }
+
+        // Create the map of the original characters to numbers
+        val originalMap = mapOf(
+            Pair("abcefg", 0),
+            Pair("cf", 1),
+            Pair("acdeg", 2),
+            Pair("acdfg", 3),
+            Pair("bcdf", 4),
+            Pair("abdfg", 5),
+            Pair("abdefg", 6),
+            Pair("acf", 7),
+            Pair("abcdefg", 8),
+            Pair("abcdfg", 9)
+        )
+        // Map each second part of a line to the original numbers
+        return@map line.last().map inner@{ entry ->
+            // Map the entry to the original characters
+            val mappedEntry = entry.map { char ->
+                charMap.filter { it.value == char.toString() }.keys.first()
+            }.map { it.toCharArray().first() }
+            // Map the list of original characters to the correct number
+            originalMap.forEach {
+                if (it.key.length == mappedEntry.size && it.key.toCharArray().toList().containsAll(mappedEntry)) {
+                    return@inner it.value
+                }
+            }
+        }.joinToString("").toInt()
+    }.sum()
+    println("Result Pt2: $resultPt2")
+
+}
