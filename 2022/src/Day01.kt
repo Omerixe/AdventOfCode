2dd31ff191e47ce2cf333433511cffcccd17450c
Jasmin <jasmin@omerixe.ch>
2022-11-28 07:46:24 +0100

    2022 prepare repo

---
+++ 2022/src/Day01.kt
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun main() {
+    val dummyInput = """
+    """.trimIndent()
+
+    val numbers = dummyInput.split("\n").map(String::toInt)
+
+    /*val numbers = File("resources/input01.txt")
+        .readLines()
+        .map(String::toInt)*/
+
+    part1()
+    part2()
+}
+
+fun part1() {
+
+}
+
+fun part2() {
+
+}
