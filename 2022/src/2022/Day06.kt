4759ec5f7f1d4c860bf75244d70e33041a593ca9
Jasmin <jasmin@omerixe.ch>
2022-12-11 16:15:14 +0100

    2022 Day 06

---
+++ 2022/src/2022/Day06.kt
@@ -0,0 +1,46 @@
+package `2022`
+
+import java.io.File
+import kotlin.math.sign
+
+fun main() {
+    val dummyInput = """
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+bvwbjplbgvbhsrlpgdmjqwftvncz
+nppdvjthqldpwncqszvftbrmjlhg
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+    """.trimIndent()
+
+    //val input = dummyInput.split("\n")
+
+    val input = File(ClassLoader.getSystemResource("2022/input06.txt").toURI())
+        .readLines()
+
+    Day06.part1(input)
+    Day06.part2(input)
+}
+
+object Day06 {
+    fun part1(input: List<String>) {
+        val res = input.map {
+            it.windowed(4)
+                    .map { element ->
+                        element.toSet().size == 4
+                    }.indexOfFirst { it } + 4
+        }
+
+        println("Part1: $res")
+    }
+
+    fun part2(input: List<String>) {
+        val res = input.map {
+            it.windowed(14)
+                    .map { element ->
+                        element.toSet().size == 14
+                    }.indexOfFirst { it } + 14
+        }
+
+        println("Part2: $res")
+    }
+}
