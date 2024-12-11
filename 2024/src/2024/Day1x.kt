51273a3a7af9815d48bf9e8e2c6ed4905192cb6e
Jasmin <jasmin@omerixe.ch>
2024-12-11 06:32:54 +0100

    Change template file for 1x days

--- 2024/src/2024/Day1x.kt
+++ 2024/src/2024/Day1x.kt
@@ -3,15 +3,15 @@ package `2024`
 import java.io.File
 
 fun main() {
-    val inputs = File(ClassLoader.getSystemResource("2024/dummy0x.txt").toURI()).readLines()
+    val inputs = File(ClassLoader.getSystemResource("2024/dummy1x.txt").toURI()).readLines()
 
-    //val inputs = File(ClassLoader.getSystemResource("2024/input0x.txt").toURI()).readLines()
+    //val inputs = File(ClassLoader.getSystemResource("2024/input1x.txt").toURI()).readLines()
 
-    Day0x.part1(inputs)
-    Day0x.part2(inputs)
+    Day1x.part1(inputs)
+    Day1x.part2(inputs)
 }
 
-object Day0x {
+object Day1x {
     fun part1(input: List<String>) {
         val result = input.map { line ->
 
